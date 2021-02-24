0de7c9be60706e9bb4d8dfe72e5a815a96ed7929
wpbindt <wesselbindt@gmail.com>
2021-02-24 13:01:35 +0100

    Refactor tests more, add cases for slices with None start and stop

--- tests.py
+++ tests.py
@@ -8,13 +8,18 @@ class TestSliceableMatrix(unittest.TestCase):
     def _check_contents(
         self,
         matrix: SliceableMatrix,
-        expected: List[List[int]]
+        expected: List[List[int]],
+        msg: str
     ) -> None:
         row_boundary = len(expected)
         col_boundary = len(expected[0])
         for row in range(row_boundary):
             for col in range(col_boundary):
-                with self.subTest('Matrix content check', row=row, col=col):
+                with self.subTest(
+                    f'Matrix content check for {msg}', 
+                    row=row, 
+                    col=col
+                ):
                     self.assertEqual(
                         matrix[row, col],
                         expected[row][col]
@@ -30,18 +35,6 @@ class TestSliceableMatrix(unittest.TestCase):
                 with self.assertRaises(IndexError):
                     matrix[row_boundary, col]
 
-
-    def test_no_slicing(self) -> None:
-        rows = [[1, 2, 3, 4],
-                [4, 5, 6, 9],
-                [8, 10, 11, 13]]
-        matrix = SliceableMatrix(rows)
-
-        self._check_contents(
-            matrix,
-            rows
-        )
-
     def test_slicing(self) -> None:
         rows = [[1, 2, 3, 4],
                 [4, 5, 6, 9],
@@ -49,13 +42,21 @@ class TestSliceableMatrix(unittest.TestCase):
         matrix = SliceableMatrix(rows)
 
         self._check_contents(
-            matrix[0:2, 1],
-            expected=[[2], [5]]
+            matrix,
+            expected=rows,
+            msg='unsliced matrix'
         )
 
         self._check_contents(
-            matrix[1, 1:4],
-            expected=[[5, 6, 9]]
+            matrix[0:2, 1],
+            expected=[[2], [5]],
+            msg='sliced rows, selected column'
+        )
+
+        self._check_contents(
+            matrix[1, 1:],
+            expected=[[5, 6, 9]],
+            msg='sliced columns, selected row'
         )
 
         self._check_contents(
@@ -63,24 +64,22 @@ class TestSliceableMatrix(unittest.TestCase):
             expected=[
                 [5, 6, 9],
                 [10, 11, 13]
-            ]
+            ],
+            msg='sliced rows and columns'
         )
 
-    def test_double_slicing(self) -> None:
-        rows = [[1, 2, 3, 4],
-                [4, 5, 6, 9],
-                [8, 10, 11, 13]]
-        matrix = SliceableMatrix(rows)
-
         self._check_contents(
-            matrix[0:3, 1:3][1:3, 0:2],
+            matrix[:3, 1:3][1:3, :2],
             expected=[
                 [5, 6],
                 [10, 11]
-            ]
+            ],
+            msg='doubly sliced matrix'
         )
 
-
-    def test_none_slice(self) -> None:
-        ...
+        self._check_contents(
+            matrix[:, :],
+            expected=rows,
+            msg='select everything with None slice'
+        )
 
