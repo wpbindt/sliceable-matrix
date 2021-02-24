d2e3f912c23c6fb3a697a2a0a63606b45bfd182f
wpbindt <wesselbindt@gmail.com>
2021-02-24 11:48:36 +0100

    Small refactor

--- sliceable_matrix.py
+++ sliceable_matrix.py
@@ -13,31 +13,38 @@ class SliceableMatrix:
             row[self._col_slice]
             for row in self.rows[self._row_slice]
         ]
-    
+
     def __repr__(self):
         return str(list(self))
 
     def __getitem__(self, index):
         if not(isinstance(index, tuple) and len(index) == 2):
             raise ValueError('Invalid slice')
-            
+
         row, col = index[0], index[1]
         if isinstance(row, int) and isinstance(col, int):
-            if not self._row_slice.start + row < self._row_slice.stop:
-                raise IndexError('Row index out of range')
-            if not self._col_slice.start + col < self._col_slice.stop:
-                raise IndexError('Column index out of range')
-            return self.rows[
-                self._row_slice.start + row
-            ][
-                self._col_slice.start + col
-            ]
-        
+            return self._get_single_item(row, col)
+
         if isinstance(row, int):
             row = slice(row, row + 1, None)
         if isinstance(col, int):
             col = slice(col, col + 1, None)
-       
+
+        return self._get_slice(row, col)
+
+    def _get_single_item(self, row: int, col: int):
+        if self._row_slice.start + row >= self._row_slice.stop:
+            raise IndexError('Row index out of range')
+        if self._col_slice.start + col >= self._col_slice.stop:
+            raise IndexError('Column index out of range')
+
+        return self.rows[
+            self._row_slice.start + row
+        ][
+            self._col_slice.start + col
+        ]
+
+    def _get_slice(self, row: slice, col: slice) -> SliceableMatrix:
         return SliceableMatrix(
             self.rows,
             col_slice=slice(
@@ -49,4 +56,3 @@ class SliceableMatrix:
                 self._row_slice.start + row.stop
             )
         )
-
