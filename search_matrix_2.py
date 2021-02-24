69905709336a483dc58a053941d141aa95e08e35
wpbindt <wesselbindt@gmail.com>
2021-02-24 22:39:56 +0100

    Add example usage

---
+++ search_matrix_2.py
@@ -0,0 +1,36 @@
+from sliceable_matrix import SliceableMatrix
+
+
+def search(matrix: SliceableMatrix[int], target: int) -> bool:
+    """
+    Souped up version of binary search for matrix
+    with ordered columns and rows.
+
+    Despite the slicing happening in the return
+    statement (which normally scales linearly with
+    the size of the matrix), this algorithm runs 
+    in O(n log(n)) time, where n is the max of 
+    the number of rows and columns.
+    It can be proved that replacing the iteration
+    over the diagonal in the beginning with something
+    that runs in logarithmic time does not improve
+    the worst case runtime. It stays O(n log(n)).
+    """
+    if not matrix:
+        return False
+    diag_sup = 0
+    while diag_sup < matrix.diag_size:
+        if matrix[diag_sup, diag_sup] > target:
+            break
+        diag_sup += 1
+
+    if diag_sup == 0:
+        return False
+    if matrix[diag_sup - 1, diag_sup - 1] == target:
+        return True
+    
+    return (
+        search(matrix[:diag_sup, diag_sup:], target)
+        or search(matrix[diag_sup:, :diag_sup], target)
+    )
+
